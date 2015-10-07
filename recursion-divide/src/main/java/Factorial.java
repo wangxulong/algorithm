2a2289be4ac93edf038b50827ec778d19b5a9fe5
wangxulong <duanxian0402@126.com>
2015-10-07 11:08:47 +0800

    撒地方

--- recursion-divide/src/main/java/Factorial.java
+++ recursion-divide/src/main/java/Factorial.java
@@ -9,7 +9,6 @@ public class Factorial {
         if(n==0) return 1;
         return n*factorial(n-1);
     }
-
     public static void main(String[] args) {
         System.out.println(factorial(5));
 
